5aef1d4ffcfff60d1b83938a1f0ca9ee0be5ae5e
diddy-bubble <laurencenakounou@gmail.com>
2022-08-19 11:21:14 +0100

    Ajoutez des fichiers projet.

---
+++ Essai1/Essai1/AppShell.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Essai1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//");
+        }
+    }
+}
